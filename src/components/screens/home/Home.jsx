16c5e2eb62aa636ebf6dab6c98e8a8b68b221a6a
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-12-02 16:13:41 +0500

    first init

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../layout/layout'
+
+function Home() {
+
+	return (
+		<Layout>
+			
+		</Layout>
+	)
+}
+
+export default Home
---
+++ src/components/screens/profile/Profile.jsx
@@ -0,0 +1,7 @@
+import Layout from '../../layout/layout'
+
+const Profile = () => {
+ return <Layout>Profile</Layout>
+}
+
+export default Profile
