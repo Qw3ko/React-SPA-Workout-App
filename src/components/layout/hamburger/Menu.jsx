e86a6c73d21b68231bcc1c12a5c28e0ae965933d
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-12-12 12:15:57 +0500

    updated menu component

--- src/components/layout/hamburger/Menu.jsx
+++ src/components/layout/hamburger/Menu.jsx
@@ -1,10 +1,27 @@
-import styles from './Hamburger.module.scss'
 import cn from 'clsx'
-import { menu } from './menu.data'
-import { Link } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { Link, useNavigate } from 'react-router-dom'
 
-const Menu = ({ isShow }) => {
-	const logoutHandler = () => {}
+import { useAuth } from '../../../hooks/useAuth'
+
+import { TOKEN } from '../../../../app.constants'
+
+import styles from './Hamburger.module.scss'
+import { menu } from './menu.data'
+
+const Menu = ({ isShow, setIsShow }) => {
+	const { setIsAuth } = useAuth()
+
+	const navigate = useNavigate()
+
+	const logoutHandler = () => {
+		Cookies.remove(TOKEN)
+		setIsAuth(false)
+		setIsShow(false)
+
+		navigate('/auth')
+	}
+
 	return (
 		<nav
 			className={cn(styles.menu, {
