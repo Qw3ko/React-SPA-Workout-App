c1e639bc5ac1898afcb71b7312411b5d7cac7104
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-12-12 12:15:40 +0500

    updated header component

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,34 +1,43 @@
 import { IoMdArrowBack } from 'react-icons/io'
 import { SlUser } from 'react-icons/sl'
 import { useLocation, useNavigate } from 'react-router-dom'
+
 import { useAuth } from '../../../hooks/useAuth'
+
 import Hamburger from '../hamburger/Hamburger'
+
 import styles from './Header.module.scss'
 
-const Header = ({ backLink = '' }) => {
+const Header = ({ backLink = '/' }) => {
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
+
 	const { isAuth } = useAuth()
+
 	return (
 		<header className={styles.header}>
-			{pathname !== '/' ? (
-				<button
-					onClick={() => {
-						navigate(backLink)
-					}}
-				>
-					<IoMdArrowBack fill='#fff' fontSize={29} />
-				</button>
-			) : (
-				<button
-					onClick={() => {
-						navigate('/profile')
-					}}
-				>
-					<SlUser fill='#fff' fontSize={27} />
-				</button>
+			{isAuth && (
+				<>
+					{pathname === '/' && isAuth ? (
+						<button
+							onClick={() => {
+								navigate('/profile')
+							}}
+						>
+							<SlUser fill='#fff' fontSize={25} />
+						</button>
+					) : (
+						<button
+							onClick={() => {
+								navigate(isAuth ? backLink : '/auth')
+							}}
+						>
+							<IoMdArrowBack fill='#fff' fontSize={29} />
+						</button>
+					)}
+					<Hamburger />
+				</>
 			)}
-			<Hamburger />
 		</header>
 	)
 }
