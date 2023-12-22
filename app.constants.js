c2ee9d8ef89a1cc2109a06d943055c8fc6dd090a
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-12-22 18:38:55 +0500

    updated constants

--- app.constants.js
+++ app.constants.js
@@ -1,2 +1,6 @@
 export const TOKEN = 'qweko'
 export const USERS = '/users'
+export const EXERCISES = '/exercises'
+export const WORKOUTS = '/workouts'
+export const EXERCISES_LOG = `${EXERCISES}/log`
+export const WORKOUT_LOG = `${WORKOUTS}/log`
